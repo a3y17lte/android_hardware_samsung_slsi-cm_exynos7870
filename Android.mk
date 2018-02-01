d170be6c789c961527ed989534bc79b01463f069
Emre Simsek <emresimsek6643@gmail.com>
2018-02-01 14:14:14 +0100

    Revert last commit

--- Android.mk
+++ Android.mk
@@ -20,7 +20,7 @@ ifeq ($(TARGET_SOC), exynos7870)
 exynos7870_dirs := \
 	mobicore \
 	gralloc \
-	libexynosdisplaymodule \
+	libdisplaymodule \
 	libhwcutilsmodule \
 	libhdmimodule \
 	libhwjpeg \
